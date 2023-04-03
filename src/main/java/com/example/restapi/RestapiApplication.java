87c43b125b919d007f905965d15040f13eb29057
anusaraweerasooriya <anusaraweerasooriya@gmail.com>
2023-04-04 00:13:30 +0530

    Created the rest api

--- src/main/java/com/example/restapi/RestapiApplication.java
+++ src/main/java/com/example/restapi/RestapiApplication.java
@@ -21,14 +21,14 @@ public class RestapiApplication {
 	}
 
 	@Bean
-	CommandLineRunner runner(StudentRepository repository, MongoTemplate mongoTemplate){
+	CommandLineRunner runner(StudentRepository repository){
 		return args -> {
 			Address address = new Address(
 					"England",
 					"London",
 					"NE9"
 			);
-			String email ="anusara@gmail.com";
+			String email ="anusar112a@gmail.com";
 			Student student = new Student(
 					 "Anusara",
 						"Weerasooriya",
@@ -40,23 +40,34 @@ public class RestapiApplication {
 					LocalDateTime.now()
 			);
 
-			Query query = new Query();
-			query.addCriteria(Criteria.where("email").is(email));
-
-			List<Student> students = mongoTemplate.find(query, Student.class);
-
-			if(students.size()>1){
-				throw new IllegalStateException("found many students with email" + email);
-			}
-
-			if(students.isEmpty()){
-				System.out.println("Added the student" + student);
-				repository.insert(student);
-			} else {
-				System.out.println(student + "already exists");
-			}
-
-
+			//usingMongoTemplateAndQuery(repository,mongoTemplate,student);
+			repository.findStudentByEmail(email)
+					.ifPresentOrElse(s -> {
+						System.out.println(student + "already exists");
+					}, ()->{
+						System.out.println("Added the student" + student);
+						repository.insert(student);
+					});
 		};
 	}
+
+//	private void usingMongoTemplateAndQuery(StudentRepository repository, MongoTemplate mongoTemplate, Student student){
+//		Query query = new Query();
+//		query.addCriteria(Criteria.where("email").is(email));
+//
+//		List<Student> students = mongoTemplate.find(query, Student.class);
+//
+//		if(students.size()>1){
+//			throw new IllegalStateException("found many students with email" + email);
+//		}
+//
+//		if(students.isEmpty()){
+//			System.out.println("Added the student" + student);
+//			repository.insert(student);
+//		} else {
+//			System.out.println(student + "already exists");
+//		}
+//
+//
+//	}
 }
