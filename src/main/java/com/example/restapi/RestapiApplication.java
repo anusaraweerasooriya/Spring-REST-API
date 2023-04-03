f52628c4424246ab450abfe976889afb1b0486ff
anusaraweerasooriya <anusaraweerasooriya@gmail.com>
2023-04-03 23:51:02 +0530

    Created a optional method to find the student by email.

--- src/main/java/com/example/restapi/RestapiApplication.java
+++ src/main/java/com/example/restapi/RestapiApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
@@ -18,17 +21,18 @@ public class RestapiApplication {
 	}
 
 	@Bean
-	CommandLineRunner runner(StudentRepository repository){
+	CommandLineRunner runner(StudentRepository repository, MongoTemplate mongoTemplate){
 		return args -> {
 			Address address = new Address(
 					"England",
 					"London",
 					"NE9"
 			);
+			String email ="anusara@gmail.com";
 			Student student = new Student(
 					 "Anusara",
 						"Weerasooriya",
-		 				"anusara@gmail.com",
+		 				email,
 						Gender.FEMALE,
 						address,
 					List.of("Computer Science"),
@@ -36,7 +40,23 @@ public class RestapiApplication {
 					LocalDateTime.now()
 			);
 
-			repository.insert(student);
+			Query query = new Query();
+			query.addCriteria(Criteria.where("email").is(email));
+
+			List<Student> students = mongoTemplate.find(query, Student.class);
+
+			if(students.size()>1){
+				throw new IllegalStateException("found many students with email" + email);
+			}
+
+			if(students.isEmpty()){
+				System.out.println("Added the student" + student);
+				repository.insert(student);
+			} else {
+				System.out.println(student + "already exists");
+			}
+
+
 		};
 	}
 }
--- src/main/java/com/example/restapi/StudentRepository.java
+++ src/main/java/com/example/restapi/StudentRepository.java
@@ -2,5 +2,9 @@ package com.example.restapi;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.Optional;
+
 public interface StudentRepository extends MongoRepository<Student, String> {
+
+    Optional<Student> findStudentByEmail(String email);
 }
